ae25f93e575d206bd18efa93f4df550a71652999
anshumandubey1 <anshuman.dubey1@thoughtworks.com>
2022-08-02 10:13:20 +0530

    Added controller to get all todos in database

--- src/main/java/com/chaosmonkey/todo/controller/TodoController.java
+++ src/main/java/com/chaosmonkey/todo/controller/TodoController.java
@@ -7,9 +7,12 @@ import com.chaosmonkey.todo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/todo")
@@ -23,5 +26,12 @@ public class TodoController {
         TodoResponse todoResponse = todoService.addTodo(todoRequest);
         ResponseDataObject<TodoResponse> response = new ResponseDataObject<>(true, todoResponse);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<ResponseDataObject<List<TodoResponse>>> list() {
+        List<TodoResponse> todoResponses = todoService.getAllTodos();
+        ResponseDataObject<List<TodoResponse>> response = new ResponseDataObject<>(true, todoResponses);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
--- src/test/java/com/chaosmonkey/todo/controller/TodoControllerTest.java
+++ src/test/java/com/chaosmonkey/todo/controller/TodoControllerTest.java
@@ -12,14 +12,16 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.doReturn;
 
 @SpringBootTest
-public class TodoControllerTest {
+class TodoControllerTest {
 
     @MockBean
     private TodoService todoService;
@@ -37,5 +39,18 @@ public class TodoControllerTest {
         ResponseEntity<ResponseDataObject<TodoResponse>> response = todoController.create(todoRequest);
 
         assertThat(response.getBody().getData(), is(equalTo(expectedTodoResponse)));
+    }
+
+    @Test
+    void shouldReturnListOfTodos() {
+        TodoResponse todo1 = new TodoResponse(1, "To do 1", "or not to do 1, there is no try!", LocalDateTime.now().plusDays(1), LocalDateTime.now(), null);
+        TodoResponse todo2 = new TodoResponse(1, "To do 2", "or not to do 2, there is no try!", LocalDateTime.now().plusDays(2), LocalDateTime.now(), null);
+        TodoResponse todo3 = new TodoResponse(1, "To do 3", "or not to do 3, there is no try!", LocalDateTime.now().plusDays(3), LocalDateTime.now(), null);
+
+        List<TodoResponse> expectedList = List.of(todo1, todo2, todo3);
+        doReturn(expectedList).when(todoService).getAllTodos();
+        ResponseEntity<ResponseDataObject<List<TodoResponse>>> response = todoController.list();
+
+        assertThat(response.getBody().getData(), containsInAnyOrder(expectedList.toArray()));
     }
 }
